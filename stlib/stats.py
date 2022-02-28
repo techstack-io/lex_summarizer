86584de8b2314e9f4c8ded210c8dcac5ab611d31
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-02-28 14:58:15 -0600

    Update stats.py

--- stlib/stats.py
+++ stlib/stats.py
@@ -11,9 +11,9 @@ def run():
          return None
      return r.json()
 
-lottie_url = "https://assets10.lottiefiles.com/packages/lf20_jYTS0r.json"
-lottie_json = load_lottieurl(lottie_url)
-st_lottie(lottie_json)
+# lottie_url = "https://assets10.lottiefiles.com/packages/lf20_jYTS0r.json"
+# lottie_json = load_lottieurl(lottie_url)
+st_lottie("https://assets10.lottiefiles.com/packages/lf20_jYTS0r.json")
 
 # end of app
 
