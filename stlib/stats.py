dd5a6a49d336447c9ef18b9e4ed3176e0dda4be3
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-02-28 15:12:12 -0600

    Update stats.py

--- stlib/stats.py
+++ stlib/stats.py
@@ -1,19 +1,18 @@
 
 def run():
  
-    import streamlit as st
-    from streamlit_lottie import st_lottie
-    from streamlit_lottie import st_lottie_spinner
-    import requests
-
-    def load_lottieurl(url: str):
-     r = requests.get(url)
-     if r.status_code != 200:
-         return None
-     return r.json()
+  import streamlit as st
+  from streamlit_lottie import st_lottie
+  import requests
+  
+  def load_lottieurl(url: str):
+      r = requests.get(url)
+      if r.status_code != 200:
+          return None
+      return r.json()
 
 lottie_url = "https://assets10.lottiefiles.com/packages/lf20_jYTS0r.json"
-lottie_json = lottie_url
+lottie_json = load_lottieurl(lottie_url)
 st_lottie(lottie_json)
 
 # end of app
