4b20c3e0f1498797cd248e7930ada4273baf5214
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-02-27 23:02:33 -0600

    Update summarizer.py

--- stlib/summarizer.py
+++ stlib/summarizer.py
@@ -2,26 +2,21 @@
 
 def run():
 
-    # Libraries
     from nltk.tokenize import word_tokenize, sent_tokenize
     import streamlit as st
     import streamlit.components.v1 as components
     from nltk.corpus import stopwords
     from typing import ByteString
-    # Chart and Plotly libs
     from wordcloud import WordCloud
     import plotly.express as px
     import chart_studio
-    # -
     import matplotlib.pyplot as plt
     import scipy as sp
     import pandas as pd
     import numpy as np
     import validators
-    # Beautiful Soup & urllib
     import bs4 as bs
     import urllib.request
-    # -
     import requests
     import string
     import heapq
@@ -45,8 +40,6 @@ def run():
      'Please select input method below:',
      ('---', 'Input Text', 'Paste a Link','Upload File'))
     
-    print(sys.executable)
-
     s_example = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
 
     # -------------------------------- Text Input -------------------------------- #
@@ -119,9 +112,9 @@ def run():
                     plt.show()
                     st.pyplot()
 
-                    # ------------------------------- Plotly Charts ------------------------------ #
-
-                    # ------------ Lists ------------- #
+                    # Plotly Charts #
+                    
+                    # --- Lists 
 
                     # Convert word2count dict to a list
                     dict2list=list(word2count.items())
@@ -132,7 +125,7 @@ def run():
                     # First 7 words in sorted list of weighted words in descending order
                     weighted_words_des = dict2list[:7]
 
-                    # ------------ Dicts --------------------- #
+                    # --- Dicts 
 
                     # Covert sorted list back to dict (this is the complete dict)
                     sortdict = dict(dict2list)
