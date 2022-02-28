7a3485b8519438b0b7a45397fb9a8361ee6655d0
techstack-io <81205746+techstack-io@users.noreply.github.com>
2022-02-28 14:55:45 -0600

    Update stats.py

--- stlib/stats.py
+++ stlib/stats.py
@@ -6,10 +6,10 @@ def run():
     from streamlit_lottie import st_lottie_spinner
 
     def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+     r = requests.get(url)
+     if r.status_code != 200:
+         return None
+     return r.json()
 
 lottie_url = "https://assets10.lottiefiles.com/packages/lf20_jYTS0r.json"
 lottie_json = load_lottieurl(lottie_url)
