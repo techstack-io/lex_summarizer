2c63d6b3203cb607d0798a1c1664ca3463d3f016
support@oliviamcreativelabs.com Collins <support@oliviamcreativelabs.com>
2022-03-01 22:20:47 -0600

    Added logo and changed homepage

--- stlib/home.py
+++ stlib/home.py
@@ -28,7 +28,7 @@ def run():
 
 
 
-    # st.header("LEX - TEXT SUMMARIZATION AND NLP ENGINE")
+    st.header("LEX - TEXT SUMMARIZATION AND NLP ENGINE")
 
     st.write("**LEX** is a text summarization tool which allows the user to copy and paste text and summarize its contents. Futhermore, LEX can scrape a webpage (website security options may prevent scraping certain websites), or accept an uploaded file and summarize its contents. This product is currently in development and it is strictly a *proof of concept* and *not optimized for any real time commercial application or insights*. If you encounter anyany inconsistency or error during the runtime, please get back to us with the error and the dataset so that it can be reproduced and solved.Submit the error message to **support@stackmetric.com** for anything more.  This app is not optimized to summarize less tha 1,000 words and other limitations apply.Let's start analyzing with LEX!.")
 
