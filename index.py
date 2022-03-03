ec62460d709ccb6b2ccb6d963341d5fe242b4a6c
support@oliviamcreativelabs.com Collins <support@oliviamcreativelabs.com>
2022-03-02 21:58:29 -0600

    Added sidebar to all pages via index.py

--- index.py
+++ index.py
@@ -16,6 +16,11 @@ import pkgutil
 import importlib
 import stlib    # default library name for apps
 
+col1, col2, col3 = st.columns([12.5,75,12.5])
+
+with col2:
+    st.image("https://lh3.googleusercontent.com/5XLeASxlnhN_3iYtrVf2Z8Dhw7QyoRKqJbVI832d8ZWM26PYM3WUb6GR_9tqw3yV_9b2YsN2vgtRqef3vEA5Aem7Gfmi-DDPkKyif-E_2fF2HjXUX3rrorB3kEcFT6C5NhBsbls=w2000")
+
 # Global arrays for holding the app names, modules and descriptions of the apps
 names = []
 modules = []
@@ -46,6 +51,19 @@ def format_func(name):
 with st.sidebar:
     st.markdown(message)
     page = st.selectbox('Select:',names, format_func=format_func) 
+    
+st.sidebar.header('Built on and with:')
+    # Badges
+with st.sidebar:
+    st.image("https://img.shields.io/pypi/pyversions/django?style=for-the-badge")
+    st.image("https://img.shields.io/conda/vn/conda-forge/python?color=green&style=for-the-badge")
+    st.image("https://img.shields.io/badge/DASHBOARDING WITH-Streamlit-green?style=for-the-badge")
+with st.sidebar.expander("NLTK"):
+    st.write("""Natural Language Toolkit is an open source Python library for Natural Language Processing.""")
+with st.sidebar.expander("Sumy"):
+    st.write("""Sumy - Simple library and command line utility for extracting summary from HTML pages or plain texts.""")
+with st.sidebar.expander("BERT"):
+    st.write("""BERT (Bidirectional tranformer) is a transformer used to overcome the limitations of RNN and other neural networks as Long term dependencies. It is a pre-trained model that is naturally bidirectional. This pre-trained model can be tuned to easily to perform the NLP tasks as specified, Summarization in our case..""")
 
 # Run the chosen app
 modules[names.index(page)].run()
--- stlib/summarizer.py
+++ stlib/summarizer.py
@@ -24,27 +24,7 @@ def run():
     import nltk
     import re
 
-    # ---------------------------------- Sidebar --------------------------------- #
-    
-    st.sidebar.header('Built on and with:')
-        # Badges
-    with st.sidebar:
-        st.image("https://img.shields.io/pypi/pyversions/django?style=for-the-badge")
-        st.image("https://img.shields.io/conda/vn/conda-forge/python?color=green&style=for-the-badge")
-        st.image("https://img.shields.io/badge/DASHBOARDING WITH-Streamlit-green?style=for-the-badge")
-    with st.sidebar.expander("NLTK"):
-        st.write("""Natural Language Toolkit is an open source Python library for Natural Language Processing.""")
-    with st.sidebar.expander("Sumy"):
-        st.write("""Sumy - Simple library and command line utility for extracting summary from HTML pages or plain texts.""")
-    with st.sidebar.expander("BERT"):
-        st.write("""BERT (Bidirectional tranformer) is a transformer used to overcome the limitations of RNN and other neural networks as Long term dependencies. It is a pre-trained model that is naturally bidirectional. This pre-trained model can be tuned to easily to perform the NLP tasks as specified, Summarization in our case..""")
-    
     # -------------------------------------Main -------------------------------------- #
-
-    col1, col2, col3 = st.columns([.25,.50,.25])
-
-    with col2:
-        st.image("https://lh3.googleusercontent.com/mztJpuCGLVw1qnlFWJZfUDFtPq0xZ37NnviJEG2Yi-ivLq3rAF-cFyZycyRZpa7-e2x86p6AD8twK0Dd5JquIf8F6gf7wXoSaxhe3-IC6Vp4LqhaZnBsRcLXuqP_XU-mc8eqTwyoIg=w2400")
 
     st.header("SUMMARIZER")
     st.write(
